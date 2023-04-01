7a5ec0b8afcf2dbf81f9a7bcccc1a4140852bbec
Ahmad <ahmad.droobi1999@gmail.com>
2023-04-01 15:05:59 +0300

    fgfg

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var e = location.href.toLowerCase();
+    if (e.endsWith(".com/")) {
+        e = e.concat("index.html")
+    }
+    $(".navigation li a").each(function() {
+        e.indexOf(this.href.toLowerCase()) > -1 && ($("li.highlight").removeClass("highlight"), $(this).parent().addClass("highlight"))
+    })
+}), $(".toggle-menu").click(function() {
+    return $(".responsive-menu").stop(!0, !0).slideToggle(), !1
+});
+
+document.body.style.zoom="90%"
